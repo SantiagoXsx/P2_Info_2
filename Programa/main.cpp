86cf88af5511b50f3a1ebad1373fc281371f7a3f
SantiagoXsx <164356222+SantiagoXsx@users.noreply.github.com>
2024-05-04 18:56:00 -0500

    Adelanto de la clase red metro

--- Programa/main.cpp
+++ Programa/main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 
 using namespace std;
-/*
 class Estacion {
 private:
     string nombre;
@@ -47,32 +46,64 @@ int main() {
 
     return 0;
 }
-*/
 
 class Linea {
-    // Suponiendo que la clase Linea ya está definida con sus propios atributos y métodos.
+private:
+    string nombre;
+
+public:
+    Linea(string _nombre) : nombre(_nombre) {}
+
+    string getNombre() const {
+        return nombre;
+    }
 };
 
 class RedMetro {
 private:
-    Linea lineas;
+    Estacion** estaciones;
+    Linea** lineas;
+    int numEstaciones;
+    int numLineas;
+    static const int MAX_ESTACIONES = 100;
+    static const int MAX_LINEAS = 10;
+
 public:
-   
-    RedMetro() {}
+    RedMetro() : estaciones(new Estacion*[MAX_ESTACIONES]), lineas(new Linea*[MAX_LINEAS]), numEstaciones(0), numLineas(0) {}
 
-    
     ~RedMetro() {
+        for (int i = 0; i < numEstaciones; ++i) {
+            delete estaciones[i];
+        }
+        delete[] estaciones;
+
+        for (int i = 0; i < numLineas; ++i) {
+            delete lineas[i];
+        }
+        delete[] lineas;
     }
 
-    void agregarLinea(const Linea& nuevaLinea) {
+    void agregarLinea(const string& nombreLinea) {
+        if (numLineas < MAX_LINEAS) {
+            lineas[numLineas++] = new Linea(nombreLinea);
+        } else {
+            cout << "No se puede agregar más líneas. Se alcanzó el límite máximo." << endl;
+        }
     }
 
-    bool eliminarLinea(const Linea& lineaAEliminar) {
+    void eliminarLinea(int indiceLinea) {
+        if (indiceLinea >= 0 && indiceLinea < numLineas) {
+            delete lineas[indiceLinea];
+            for (int i = indiceLinea; i < numLineas - 1; ++i) {
+                lineas[i] = lineas[i + 1];
+            }
+            numLineas--;
+        } else {
+            cout << "Índice de línea inválido." << endl;
+        }
     }
 
-    int obtenerNumeroDeLineas() const {
-    }
-
-    int obtenerNumeroTotalDeEstaciones() const {
+    int numeroLineas() const {
+        return numLineas;
     }
 };
